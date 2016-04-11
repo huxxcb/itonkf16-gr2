4d0b461295f7c3315ca519cd17b0d1acf2e44649
Mat-Hapahapa <matry7@gmail.com>
2016-04-11 14:21:48 +0200

    First example commit

--- boot_example_Group2/src/main/java/Grup2/BootExampleGroup2Application.java
+++ boot_example_Group2/src/main/java/Grup2/BootExampleGroup2Application.java
@@ -1,12 +1,30 @@
 package Grup2;
 
+import Grup2.repositories.PieRepository;
+import com.github.javafaker.Faker;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.*;
 
 @SpringBootApplication
 public class BootExampleGroup2Application {
 
+	private final Faker faker = new Faker();
+
 	public static void main(String[] args) {
 		SpringApplication.run(BootExampleGroup2Application.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner initializeDb(PieRepository repository){
+		return (args) -> {
+			repository.deleteAll();
+			//Insert some random pies
+			for(int i = 0; i < 20; i++) {
+				repository.save(new Pie(faker.lorem().word(), faker.lorem().sentence()));
+			}
+		};
 	}
 }
