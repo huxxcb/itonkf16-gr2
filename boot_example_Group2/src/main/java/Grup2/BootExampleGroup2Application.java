21c96711734a404f5c234c2a51c8d7b33c5ead4c
methgaard <methgaard@gmail.com>
2016-04-11 13:04:57 +0200

    Committed project

---
+++ boot_example_Group2/src/main/java/Grup2/BootExampleGroup2Application.java
@@ -0,0 +1,12 @@
+package Grup2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootExampleGroup2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootExampleGroup2Application.class, args);
+	}
+}
