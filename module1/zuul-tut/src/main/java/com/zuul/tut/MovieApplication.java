46e15ab344a37c2f8f63f0440cb54202027e1860
huxxcb <hakonprimdahl@gmail.com>
2016-04-14 09:21:31 +0200

    moved module1 apps to module1 folder

--- module1/zuul-tut/src/main/java/com/zuul/tut/MovieApplication.java
+++ module1/zuul-tut/src/main/java/com/zuul/tut/MovieApplication.java
@@ -2,8 +2,10 @@ package com.zuul.tut;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class MovieApplication {
 
 	public static void main(String[] args) {
