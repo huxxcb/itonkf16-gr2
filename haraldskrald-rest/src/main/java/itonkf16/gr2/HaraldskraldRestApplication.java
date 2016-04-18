ad67fbd96e4898eb50414b1fe31ec3f8b1c3c1e5
mikje53 <im.mikkel@gmail.com>
2016-04-18 04:03:21 -0700

    Added source files

---
+++ haraldskrald-rest/src/main/java/itonkf16/gr2/HaraldskraldRestApplication.java
@@ -0,0 +1,12 @@
+package itonkf16.gr2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaraldskraldRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaraldskraldRestApplication.class, args);
+	}
+}
