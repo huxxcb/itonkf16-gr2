76ba167101584f3310325b609ffe87a58362335b
huxxcb <hakonprimdahl@gmail.com>
2016-04-25 14:44:03 +0200

    enabled zuul proxy

--- Gateway-ui/src/main/java/com/haraldskrald/GatewayUiApplication.java
+++ Gateway-ui/src/main/java/com/haraldskrald/GatewayUiApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class GatewayUiApplication {
 
 	public static void main(String[] args) {
