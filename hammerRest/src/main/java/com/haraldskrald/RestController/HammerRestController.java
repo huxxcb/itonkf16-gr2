8b7e7c7372321aac1bcd08f15e8500e40430b3b0
methgaard <methgaard@gmail.com>
2016-04-28 11:40:04 +0200

    fixed hammers

--- hammerRest/src/main/java/com/haraldskrald/RestController/HammerRestController.java
+++ hammerRest/src/main/java/com/haraldskrald/RestController/HammerRestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Collection;
 
 @RestController
-@RequestMapping("/Hammers")
+@RequestMapping("/hammers")
 public class HammerRestController {
     @Autowired
     private HammerRepository repository;
