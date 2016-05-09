d0f021081735fb38d49e4e49edb26ee221883fb6
methgaard <methgaard@gmail.com>
2016-05-09 15:01:16 +0200

    setup ui in Angular

---
+++ gateway-ui/src/main/resources/static/js/gateway.js
@@ -0,0 +1,13 @@
+angular.module('gateway', []).controller('navigation',
+    function($http) {
+        var self = this;
+
+        self.template = "home.html";
+        self.home = function(){
+            self.template = "home.html";
+        }
+
+        self.hammers = function(){
+            self.template = "hammers.html";
+        };
+    });
